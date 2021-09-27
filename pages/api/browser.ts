e06db55a89158743934fe1c5e6e8e465ec6fa446
hardcorequal <hardcorequal@gmail.com>
2021-09-27 10:26:24 +0300

    fix: locally run playwright from devDependency,  onVercel -> chrome-aws-lambda

--- pages/api/browser.ts
+++ pages/api/browser.ts
@@ -2,15 +2,15 @@ import chromium  from 'chrome-aws-lambda';
 import playwright from 'playwright-core';
 
 export default async function (req: any, res: any) {
-  const browser = await playwright.chromium.launch({
+  const isOnVercel = chromium.headless;
+
+  const browser = await playwright.chromium.launch(isOnVercel ? {
     args: chromium.args,
     executablePath: await chromium.executablePath,
     headless: chromium.headless,
-  });
-  const context = await browser.newContext();
+  } : {});
 
-
-  const page = await context.newPage();
+  const page = await browser.newPage();
   await page.goto('https://google.com/');
   const pdf = await page.pdf();
 
