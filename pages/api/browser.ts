94e2e8e56b95e9b15aeb34a8396107351e900f66
hardcorequal <hardcorequal@gmail.com>
2021-09-27 10:16:52 +0300

    fix: change example for deploy on vercel #1

--- pages/api/browser.ts
+++ pages/api/browser.ts
@@ -1,12 +1,16 @@
-const chromium = require('chrome-aws-lambda');
-const playwright = require('playwright-core');
+import chromium  from 'chrome-aws-lambda';
+import playwright from 'playwright-core';
 
 export default async function (req: any, res: any) {
-  const browser = await playwright.chromium.launch({});
+  const browser = await playwright.chromium.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  });
   const context = await browser.newContext();
 
 
-  const page = await context.newPage({});
+  const page = await context.newPage();
   await page.goto('https://google.com/');
   const pdf = await page.pdf();
 
